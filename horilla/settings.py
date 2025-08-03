1f0d21218c66c7a6157a31519d49e68a326445ed
Captainsparrow404 <captainsparrow2814@gmail.com>
2025-08-04 03:36:52 +0530

    settings.py

--- horilla/settings.py
+++ horilla/settings.py
@@ -70,7 +70,7 @@ INSTALLED_APPS = [
     "payroll",
     "widget_tweaks",
     "django_apscheduler",
-    "horilla_automations",
+    "horilla_automations.apps.HorillaAutomationConfig",
 ]
 APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
 
