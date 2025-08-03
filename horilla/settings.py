98e503effde1eeabc39906f5b9bb6b1589a0f73b
Captainsparrow404 <captainsparrow2814@gmail.com>
2025-08-04 03:25:54 +0530

    settings.py

--- horilla/settings.py
+++ horilla/settings.py
@@ -166,8 +166,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
